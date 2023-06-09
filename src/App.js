0ee8e5157ca5e95a50e4e3a4df1b4c43608eb9e4
mrkakawiteo <loveyourswain@gmail.com>
2023-06-09 11:35:16 +0300

    Lab in React.js with some CSS

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import './App.css'
 
 function App() {
+  const [allAlbums, setAllAlbums] = useState([]);
+  const [focusedAlbum, setFocusedAlbum] = useState(null);
+
+    fetch('https://jsonplaceholder.typicode.com/albums')
+        .then(response => response.json())
+        .then(data => setAllAlbums(data))
+        .catch(error => console.log(error));
+
+  const parsePhotos = (albumId) => {
+    fetch(`https://jsonplaceholder.typicode.com/albums/${albumId}/photos`)
+        .then(res =>res.json())
+        .then(photos => setFocusedAlbum(photos))
+        .catch(error => console.log(error));
+  };
+
+  const closePopup = () => {
+    setFocusedAlbum(null);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <h1>Albums</h1>
+        <div className="album-grid">
+          {allAlbums.map((album) => (
+              <div key={album.id} className="album-card" onClick={() => parsePhotos(album.id)}>
+                <h2>{album.title}</h2>
+              </div>
+          ))}
+        </div>
+        {focusedAlbum && (
+            <div className="popup">
+              <div className="popup-content">
+                <div className="photo-grid">
+                  {focusedAlbum.map((photo) => (
+                      <div key={photo.id} className="photo-card">
+                        <img src={photo.thumbnailUrl} alt={photo.title} />
+                        <p>{photo.title}</p>
+                      </div>
+                  ))}
+                </div>
+                <button onClick={closePopup}>Close Album</button>
+              </div>
+            </div>
+        )}
+      </div>
   );
 }
 
